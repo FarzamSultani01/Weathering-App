8a284974811bc133211a874950775db79e88e3ce
FarzamSultani01 <7my6r9mqwn@privaterelay.appleid.com>
2026-05-10 01:19:39 +0430

    Done

--- clock-weather-app/src/App.jsx
+++ clock-weather-app/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { motion} from 'framer-motion'
 import './App.css'
 import Clock from './Components/Clock.jsx'
 import Weather from './Components/Weather.jsx'
@@ -7,24 +8,46 @@ import bg_day from './assets/desktop/bg-image-daytime.jfif'
 import bg_night from '../src/assets/desktop/bg-night.jpg'
 
 function App() {
+  const [expanded, setExpanded] = useState(false);
  const [bgImage, setBgImage] = useState(bg_day);
 
  useEffect(()=>{
   const hour = new Date().getHours();
   const isDay = hour >= 6 && hour < 18;
   setBgImage(isDay ? bg_day : bg_night);
- })
+ },[])
 
 
   return (
     <>
     <div className="background">
       <img src={bgImage} alt="background-day" className='bg-day' />
-      <div className="content">
-      <Motivation />
+      <motion.div className="content"
+        initial={{ opacity: 1, y: 50}}  
+        animate={{ opacity: 1, y: 0 }}    
+        transition={{ duration: 1, ease: "easeOut" }} 
+      >
+     {!expanded &&
+     <motion.div
+      initial={{ opacity: 0, y: 30 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.9 }}>
+     <Motivation />
+     </motion.div> }
+     <motion.div
+      initial={{ opacity: 0, y: 30 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.6 }}>
       <Weather />
-      <Clock />
-      </div>
+      </motion.div>
+       <motion.div
+          initial={{ opacity: 0, y: 30 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.9 }}
+        >
+      <Clock expanded={expanded} setExpanded={setExpanded}/>
+     </motion.div>
+      </motion.div>
       </div>
    
    </>
--- clock-weather-app/src/Components/Clock.jsx
+++ clock-weather-app/src/Components/Clock.jsx
@@ -1,12 +1,16 @@
 import {useState,useEffect} from 'react'
 import './Clock.css'
+import {motion} from 'framer-motion'
 import sunIcon from '../assets/desktop/icon-sun.svg'
 import moonIcon from '../assets/desktop/icon-moon.svg'
 import arrowDown from '../assets/desktop/icon-arrow-down.svg'
+import arrowUp from '../assets/desktop/icon-arrow-up.svg'
 
 
-function Clock() {
+
+function Clock({expanded,setExpanded}) {
 const [time, setTime] = useState(new Date());
+
 
   useEffect(()=>{
     const timer =setInterval(()=>{
@@ -24,17 +28,46 @@ const [time, setTime] = useState(new Date());
     hour12: true
   })
   const city = "In Kabul , Afghanistan";
+  const timezone = "Asia/Kabul";
+  const dayOfWeek = time.getDay();
+  const dayOfYear = Math.floor((time - new Date(time.getFullYear(), 0, 0)) / 86400000);
+  const weekNumber = Math.ceil(dayOfWeek / 7);
   return (
-    <div className="clock_continer">
+    <div className={`clock_continer ${expanded ? "expanded" : ""}`}>
         <div className="clock_contents">
+        
         <p className='message'><img src={icon} alt="icon day/night" /><span>{message}</span></p>
+       
         <h1 className='timeShow'>{timeFormat}</h1>
         <p className='city'>{city}</p>
         <div className="btn"> 
-          <button>More <img src={arrowDown} alt="" /></button>
+          <button onClick={()=> setExpanded(!expanded)}>{expanded ? "Less" : "More"}{""} <img src={expanded ? arrowUp : arrowDown} alt="" /></button>
         </div>
-        
         </div>
+        {expanded && (
+          <motion.div className="expanded_info"
+    initial={{ opacity: 0, y: 50 }}  
+    animate={{ opacity: 1, y: 0 }}    
+    exit={{ opacity: 0, y: 50 }}      
+    transition={{ duration: 0.5, ease: "easeOut" }}>
+            <div>
+              <p className='lable'>Current Time Zone</p>
+              <h2>{timezone}</h2>
+            </div>
+            <div>
+              <p className='lable'>Day of the week</p>
+              <h2>{dayOfWeek}</h2>
+            </div>
+            <div>
+              <p className='lable'>Day of the year</p>
+              <h2>{dayOfYear}</h2>
+            </div>
+            <div>
+              <p className='label'> Week Number </p>
+              <h2>{weekNumber}</h2>
+            </div>
+           </motion.div>
+            )}
       </div>
   )
 }
